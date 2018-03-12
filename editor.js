7c331bb8502e829d2960f83e02c868084a1debb4
Ragekit <bgattet@gmail.com>
2018-03-11 20:32:10 -0400

    toward text style (broken commit)

--- editor.js
+++ editor.js
@@ -1,60 +1,76 @@
-var browser = browser || chrome;
+let ed1, ed2;
 
 document.addEventListener("DOMContentLoaded", function (event) {
-    var ed1 = new Editor(document.getElementById("editor"),"data");
-    var ed2 = new Editor(document.getElementById("scratchpad"),"notes");
+    ed1 = new Editor(document.getElementById("editor"), "data");
+    ed2 = new Editor(document.getElementById("scratchpad"), "notes");
+});
+
+document.addEventListener("visibilitychange", function (e) {
+    if (!document.hidden) {
+        //reload text if tab is becoming visible
+        ed1.load();
+        ed2.load();
+    }
 });
 
 
-function Editor(htmlelement, localStorageKey) {
-   
-    this.htmlElement = htmlelement;
-    this.localStorageKey = localStorageKey;
-    this.data = {};
-    this.save = function () {
+class Editor {
+    constructor(htmlElement, localStorageKey) {
+        this.htmlElement = htmlElement;
+        this.localStorageKey = localStorageKey;
+        this.lines = [];
+        this.data = {};
+        this.htmlElement.addEventListener('keydown', function (e) {
+            if (e.keyCode == 9) {
+                e.preventDefault()
+                //add tab
+                document.execCommand('insertHTML', false, '&#009');
+                //prevent focusing on next element       
+            }
+
+        }.bind(this));
+
+        this.htmlElement.addEventListener('keyup', function (e) {
+            this.format()
+            this.save();
+
+        }.bind(this));
+
+        this.load();
+        this.htmlElement.focus();
+    }
+
+    format() {
+        console.log(this.htmlElement.textContent);
+        this.lines = this.htmlElement.innerHTML.split("\n");
+        
+        console.log(this.lines);
+        for (let index = 0; index < this.lines.length; index++) {
+            let line = this.lines[index];
+
+            line = "<p>" + line + "</p>";
+            this.lines[index] = line;
+        }
+        console.log(this.lines);
+        this.htmlElement.innerHTML = this.lines;
+    }
+
+
+    save() {
         this.data.content = this.htmlElement.innerHTML;
         this.data.edited = (new Date()).getTime();
-        localStorage.setItem(this.localStorageKey,JSON.stringify(this.data));
-    }
-    this.load = function(){
-       this.data = JSON.parse(localStorage.getItem(this.localStorageKey));
-
-       //console.log(this.data.content);
-            if (!this.data) {
-                this.data = { edited: (new Date()).getTime(), content: 'Write Here' };
-            }
-            this.htmlElement.innerHTML = this.data.content;
+        localStorage.setItem(this.localStorageKey, JSON.stringify(this.data));
     }
 
-    this.htmlElement.addEventListener('keydown', function (e) {
-        //console.log(e);
-        if(e.keyCode == 9){
-            e.preventDefault()
-            //add tab
-            document.execCommand('insertHTML', false, '&#009');
-            //prevent focusing on next element
-               
+    load() {
+        this.data = JSON.parse(localStorage.getItem(this.localStorageKey));
+
+        //console.log(this.data.content);
+        if (!this.data) {
+            this.data = { edited: (new Date()).getTime(), content: '<p>Write Here</p>' };
         }
-        
-    }.bind(this));
-
-    this.htmlElement.addEventListener('keyup',function(e){
-        this.save();
-
-    }.bind(this));
-
-    document.addEventListener("visibilitychange",function(e){
-        if(!document.hidden){
-            //reload text if tab is becoming visible
-            this.load();
-        }
-    }.bind(this));
-
-    
-    this.load();
-    
-    
-    this.htmlElement.focus();
+        this.htmlElement.innerHTML = this.data.content;
+    }
 }
 
 
