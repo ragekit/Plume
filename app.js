c4798d1f736ba6ce6ef03ce42e150f1b58343fb2
Benjamin Gattet <bgattet@gmail.com>
2019-08-17 16:18:58 -0400

    clean app.js

--- app.js
+++ app.js
@@ -5,32 +5,6 @@ const bodyParser = require('body-parser')
 const fs = require("fs").promises;
 let jsonparser = bodyParser.json();
 app.use(express.static('public'));
-app.use((req, res, next) => {
-
-    // -----------------------------------------------------------------------
-    // authentication middleware
-  
-    const auth = {login: 'yourlogin', password: 'yourpassword'} // change this
-  
-    // parse login and password from headers
-    const b64auth = (req.headers.authorization || '').split(' ')[1] || ''
-    const [login, password] = Buffer.from(b64auth, 'base64').toString().split(':')
-  
-    // Verify login and password are set and correct
-    if (login && password && login === auth.login && password === auth.password) {
-      // Access granted...
-      return next()
-    }
-  
-    // Access denied...
-    res.set('WWW-Authenticate', 'Basic realm="401"') // change this
-    res.status(401).send('Authentication required.') // custom message
-  
-    // -----------------------------------------------------------------------
-  
-  })
-
-
 
 app.get('/', (req, res) => res.send("index.html"))
 
@@ -44,8 +18,6 @@ app.post("/update",jsonparser,(req,res)=>{
 
 app.get('/data',(req,res) =>{
     res.sendFile(__dirname+"/data/plume.json")
-    console.log(req.header("Host"))
-    console.log(req.header("Origin"))
 } );
 
 function prepareData(data,year,month,day){
@@ -60,7 +32,7 @@ let fullData;
 fs.readFile(__dirname+"/data/plume.json","utf8")
 .then((data,err) => fullData = JSON.parse(data))
 .then(()=>{
-    app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+    app.listen(port, () => console.log(`Plume listening on port ${port}!`))
 })
 
 
